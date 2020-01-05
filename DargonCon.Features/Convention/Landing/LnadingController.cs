ec1a20967202292441f1e3a934ab399637b81547
Eliran Teller <themadkow@gmail.com>
2020-01-05 13:23:48 +0200

    Started Front-end Styling

---
+++ DargonCon.Features/Convention/Landing/LnadingController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DragonCon.Features.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DragonCon.Features.Convention.Landing
+{
+    [Area("Convention")]
+    public class LandingController : DragonController<NullGateway>
+    {
+        public LandingController(IServiceProvider service) : 
+            base(service)  { }
+
+        [HttpGet("/Landing")]
+        [HttpGet("/Convention/Landing")]
+        [HttpGet("/Convention/Landing/Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
