32ac3db3208045613f39c10bb256d096e7ed71c2
Eliran Teller <themadkow@gmail.com>
2020-01-13 23:41:40 +0200

    Sponsors & Could Data

---
+++ DragonCon.Modeling/Models/UserDisplay/DynamicSponsorItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DragonCon.Modeling.Models.UserDisplay
+{
+    public class DynamicSponsorItem : DynamicDisplayItem
+    {
+        public string ImageUrl { get; set; } = string.Empty;
+        public string SponsorUrl { get; set; } = string.Empty;
+        public string Caption { get; set; } = string.Empty;
+    }
+}
